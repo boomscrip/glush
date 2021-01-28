2d1a852885d7e4d2a9ac4544d649062a9442d0f7
Alex <alex@MacBook-Pro-Alex.local>
2021-01-28 20:46:03 +0300

    tree

--- my.js
+++ my.js
@@ -1,2 +1,5 @@
-var xme = document.querySelector(".t-tildalabel");
-console.log(xme);
+// document.querySelector(".t-tildalabel").remove();
+//
+window.onload = function() {
+  var xme = document.querySelector(".t-tildalabel").remove();
+}
