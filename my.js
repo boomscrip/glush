989749f6ddcbd0dde54a481fa0ce8722cd3c93dd
Alex <alex@MacBook-Pro-Alex.local>
2021-01-28 21:01:29 +0300

    t

--- my.js
+++ my.js
@@ -1,12 +1,13 @@
 // document.querySelector(".t-tildalabel").remove();
 //
 window.onload = function() {
-  var xme = document.querySelector(".t-tildalabel").remove();
+  // var xme = document.querySelector(".t-tildalabel").remove();
+    var xme = document.querySelector(".t-tildalabel").style.display = 'none';
   restore();
 }
 function restore() {
 
   // document.querySelectorAll("div.t-tildalabel").remove();
 
-  document.querySelectorAll(".t-tildalabel").forEach(e => e.parentNode.removeChild(e));
+  // document.querySelectorAll(".t-tildalabel").forEach(e => e.parentNode.removeChild(e));
 }
